2e4693cf300014950d0ca0147e55fd942ec205fa
Gboyega Temiloluwa Marvelous <158356260+Temiloluwagboyega@users.noreply.github.com>
2024-10-22 04:38:14 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,25 +6,6 @@ export default {
   ],
   theme: {
     extend: {},
-    screens: {
-      '2xl': {'max': '1535px'},
-      // => @media (max-width: 1535px) { ... }
-  
-      'xl': {'max': '1030px'},
-      // => @media (max-width: 1279px) { ... }
-  
-      '2lg': {'max': '990px'},
-      // => @media (max-width: 1023px) { ... }
-
-      'lg': {'max': '930px'},
-      // => @media (max-width: 1023px) { ... }
-  
-      'md': {'max': '800px'},
-      // => @media (max-width: 767px) { ... }
-  
-      'sm': {'max': '639px'},
-      // => @media (max-width: 639px) { ... }
-    }
   },
   plugins: [],
 
